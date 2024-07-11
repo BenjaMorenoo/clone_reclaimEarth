05037ac37d7ad977a1c372a43ed57e94b7926b9e
BenjaMorenoo <benjaben100@gmail.com>
2024-07-10 22:58:14 -0400

    Clon de ReclaimEarth Este proyecto es un clon de la página de ReclaimEarth, una plataforma de reciclaje que fomenta este hábito en la comunidad. Mediante recompensas para usuarios activos, busca incentivar el reciclaje y la conciencia ambiental.
    Tecnologías utilizadas
    - Astro
    - TailwindCSS
    - TypeScript
    
    Avance del proyecto:
    - [x] Crear la estructura de carpetas
    - [x] Crear los componentes
    - [x] Crear el layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				verdeMain: "#E0F7E9",
+				verdeNavbar: "#28A745",
+			},
+		},
+	},
+	plugins: [],
+}
